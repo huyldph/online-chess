09ed9b928e8bc061cbf72a7434701723eee56b84
huyldph <ledinhhuy26032004@gmail.com>
2025-04-25 17:58:51 +0700

    feat(frontend): add configuration for OAth2 with Google

---
+++ frontend/src/configurations/configuration.js
@@ -0,0 +1,5 @@
+export const OAuthConfig = {
+    clientId: "927301257409-cauirmripm0a1odha54rg22anhe8dn5f.apps.googleusercontent.com",
+    redirectUri: "http://localhost:5173/authenticate",
+    authUri: "https://accounts.google.com/o/oauth2/auth",
+};
