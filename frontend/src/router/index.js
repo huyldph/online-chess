80999ba3e38a7a7d01500ef83e9161bb223a4db8
huyldph <ledinhhuy26032004@gmail.com>
2025-04-26 16:30:26 +0700

    feat(frontend): update route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../components/Login.vue'
 import Register from '../components/Register.vue'
+import Home from "../components/Home.vue";
+import Authenticate from "../components/Authenticate.vue";
+import { getToken } from '../services/localStorageService';
 
 const routes = [
   {
     path: '/',
-    redirect: '/login'
+    name: 'Home',
+    component: Home,
+    meta: { requiresAuth: true }
   },
   {
     path: '/login',
@@ -16,6 +21,11 @@ const routes = [
     path: '/register',
     name: 'Register',
     component: Register
+  },
+  {
+    path: '/authenticate',
+    name: 'Authenticate',
+    component: Authenticate,
   }
 ]
 
@@ -23,5 +33,18 @@ const router = createRouter({
   history: createWebHistory(),
   routes
 })
+
+// Navigation guard
+router.beforeEach((to, from, next) => {
+  const token = getToken();
+  
+  if (to.meta.requiresAuth && !token) {
+    next('/login');
+  } else if (token && to.path === '/login') {
+    next('/');
+  } else {
+    next();
+  }
+});
 
 export default router 
