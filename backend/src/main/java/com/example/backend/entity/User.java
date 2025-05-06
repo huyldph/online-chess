7acc095c5959be02432fee7783f55f7250681c87
huyldph <ledinhhuy26032004@gmail.com>
2025-05-06 21:27:45 +0700

    feat(Backend): add new properties

--- backend/src/main/java/com/example/backend/entity/User.java
+++ backend/src/main/java/com/example/backend/entity/User.java
@@ -25,6 +25,9 @@ public class User {
     String firstName;
     LocalDate dob;
     String lastName;
+    String fullName;
+    String avatar;
+    String provider;
 
     @ManyToMany
     Set<Role> roles;
