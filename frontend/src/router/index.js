ff1f73fe2b7a8a88421c7befa15b03c7ebef0d69
huyldph <ledinhhuy26032004@gmail.com>
2025-05-08 13:51:46 +0700

    feat(frontend): add router for Profile and Rankings

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,8 @@ import Login from '../components/Login.vue'
 import Register from '../components/Register.vue'
 import Home from "../components/Home.vue";
 import Authenticate from "../components/Authenticate.vue";
+import Profile from "../components/Profile.vue";
+import Rankings from "../components/Rankings.vue";
 import {getToken} from '../services/localStorageService';
 
 const routes = [
@@ -26,6 +28,24 @@ const routes = [
         path: '/authenticate',
         name: 'Authenticate',
         component: Authenticate,
+    },
+    {
+        path: '/profile',
+        name: 'Profile',
+        component: Profile,
+        meta: {requiresAuth: true}
+    },
+    {
+        path: '/profile/:username',
+        name: 'UserProfile',
+        component: Profile,
+        meta: {requiresAuth: true}
+    },
+    {
+        path: '/rankings',
+        name: 'Rankings',
+        component: Rankings,
+        meta: {requiresAuth: true}
     }
 ]
 
