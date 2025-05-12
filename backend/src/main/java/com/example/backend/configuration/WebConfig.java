cd61435f83825fa4e8bbda7aec21f306c9e6a119
huyldph <ledinhhuy26032004@gmail.com>
2025-05-12 10:28:16 +0700

    feat(backend): format code

--- backend/src/main/java/com/example/backend/configuration/WebConfig.java
+++ backend/src/main/java/com/example/backend/configuration/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/identity/**")  // Đảm bảo là /identity/** nếu context-path là /identity
+        registry.addMapping("/identity/**") // Đảm bảo là /identity/** nếu context-path là /identity
                 .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
